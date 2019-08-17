a7a411f9f57e62d6f2e9e553affa3db362d27dbb
gscheiderle <info@gscheiderle.de>
2019-08-17 15:42:20 +0200

    local einloggen

--- intern/mysql_connect_gscheiderle.php
+++ intern/mysql_connect_gscheiderle.php
@@ -1,10 +1,10 @@
 <?php
-$link=mysqli_connect("mysql5.gscheiderle.de","db562163_1","339Us%&/6815","db562163_1") or die
+/*$link=mysqli_connect("mysql5.gscheiderle.de","db562163_1","339Us%&/6815","db562163_1") or die
   ("Keine Verbindung moeglich");
+*/
 
-
-/*$link=mysqli_connect("localhost","root","","gscheiderle") or die
-  ("Keine Verbindung moeglich");*/
+$link=mysqli_connect("localhost","root","","gscheiderle") or die
+  ("Keine Verbindung moeglich");
 
   
   $zeit=date("Y-m-d H:i:s");
